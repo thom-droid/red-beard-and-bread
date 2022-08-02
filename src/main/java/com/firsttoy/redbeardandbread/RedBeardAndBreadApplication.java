758b001fb80358b42de81b8f30b2c85f0f5313f7
thom-window <hsbang.thom@gmail.com>
2022-08-02 17:34:49 +0900

    Initial commit

---
+++ src/main/java/com/firsttoy/redbeardandbread/RedBeardAndBreadApplication.java
@@ -0,0 +1,13 @@
+package com.firsttoy.redbeardandbread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBeardAndBreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedBeardAndBreadApplication.class, args);
+	}
+
+}
