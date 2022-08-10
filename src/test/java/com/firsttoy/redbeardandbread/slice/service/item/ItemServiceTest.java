427ba857ff63aed6364a2fea6d637e4831942e8b
thom <thom-droid@github.com>
2022-08-10 17:42:18 +0900

    test (item) : slice test for item
    - Add a test for repository to check cascading options in persistence context.
    - Add a service slice test for runtime exception.

---
+++ src/test/java/com/firsttoy/redbeardandbread/slice/service/item/ItemServiceTest.java
@@ -0,0 +1,28 @@
+package com.firsttoy.redbeardandbread.slice.service.item;
+
+import com.firsttoy.redbeardandbread.item.entity.Item;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemServiceTest extends ServiceTestConfig {
+
+    @Test
+    public void throwsRuntimeExceptionWhenCreateItem() {
+
+        Item item = Item.builder().code("CROI").build();
+        given(itemRepository.findByCode(item.getCode())).willReturn(Optional.of(item));
+
+        assertThrows(RuntimeException.class, () -> itemService.createItem(item));
+
+    }
+
+    @Test
+    public void createItem() {
+
+    }
+}
